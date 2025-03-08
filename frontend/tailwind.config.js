c90850ca92fddfb9d666ed8fd73f3d567fb89a61
Code10199 <danaolumad@gmail.com>
2025-03-08 11:19:50 +0800

    topnav ui

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,12 +1,67 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+module.exports = {
+  purge: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@mui/**/*.{js,ts,jsx,tsx}",
+  ],
+  darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {
-      colors: {
-        "juan-black": "#292928",
-      },
+    screens: {
+      sm: "576px",
+      md: "768px",
+      lg: "992px",
+      xl: "1280px",
     },
+    fontFamily: {
+      sans: [
+        "Jost",
+        "ui-sans-serif",
+        "system-ui",
+        "-apple-system",
+        "BlinkMacSystemFont",
+        '"Segoe UI"',
+        "Roboto",
+        '"Helvetica Neue"',
+        "Arial",
+        '"Noto Sans"',
+        "sans-serif",
+        '"Apple Color Emoji"',
+        '"Segoe UI Emoji"',
+        '"Segoe UI Symbol"',
+        '"Noto Color Emoji"',
+      ],
+    },
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+
+      white: "#FFFFFF",
+
+      gray100: "#EEEEEE",
+      gray200: "#ECECEC",
+      gray300: "#C1C1C1",
+      gray400: "#686868",
+      gray500: "#282828",
+
+      red: "#F05454",
+      pink: "#F2EEEF",
+      yellow: "#F5B461",
+      green: "#9BDEAC",
+      blue: "#66BFBF",
+      lightgreen: "#F2FDFB",
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {
+      transform: ["group-hover"],
+      scale: ["group-hover"],
+      transitionDuration: ["group-hover"],
+      letterSpacing: ["group-hover"],
+      width: ["group-hover"],
+      borderColor: ["group-hover"],
+    },
+    // divideColor: ['group-hover'],
   },
   plugins: [],
 };
